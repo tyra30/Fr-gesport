d2f6b32361edcf7b2d28188d90712ac44946516a
Tyra <ekstrandtyra@gmail.com>
2020-10-12 09:17:46 +0200

    add question 2

--- frågesport/Program.cs
+++ frågesport/Program.cs
@@ -8,6 +8,8 @@ namespace frågesport
         {
 
             string answer1;
+            string answer2;
+            int points = 0;
 
             Console.WriteLine("Hello, and welcome to the Rick and Morty quiz! You will be met by 5 questions and 3 options for each question, a, b, or c. Only one option is right and you will have to use your rick and morty knowledge to select the right option.");
 
@@ -24,6 +26,7 @@ namespace frågesport
             if (answer1 == "a")
             {
                 Console.WriteLine("Correct!");
+                points = points + 1;
 
             }
 
@@ -41,9 +44,38 @@ namespace frågesport
 
             }
 
+            Console.ReadLine();
+            Console.WriteLine("Question 2: How many season are there in Rick and Morty? a) 5	b) 2	c) 4");
+
+            answer2 = Console.ReadLine().ToLower();
+            while (answer2 != "a" && answer2 != "b" && answer2 != "c")
+            {
+                System.Console.WriteLine("Please select one of the options: a, b, or c");
+                answer2 = Console.ReadLine().ToLower();
+            }
+
+            if (answer2 == "a")
+            {
+                Console.WriteLine("WRONG!");
+
+            }
+
+
+            else if (answer2 == "b")
+            {
+                Console.WriteLine("WRONG!");
+
+            }
+
+
+            else if (answer2 == "c")
+            {
+                Console.WriteLine("Correct!");
+                points = points + 1;
+
+            }
 
             Console.ReadLine();
-
         }
     }
 }
