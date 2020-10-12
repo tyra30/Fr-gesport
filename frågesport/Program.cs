8ef89cb5128afaac27edba90fe7987e300544697
Tyra <ekstrandtyra@gmail.com>
2020-10-12 08:34:42 +0200

    first commit

---
+++ frågesport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
